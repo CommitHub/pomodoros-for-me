0bd3776c6e42ab762f1610acdfa110ed223234ca
Luis Alvarez <luis.alvarez@commithub.com>
2020-02-17 15:33:07 -0500

    added footer with a nice message

---
+++ fe/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">
+    <p>
+      Made with 💖 by Luis Alvarez
+      <a href="https://commithub.com" target="_blank" rel="noopener">
+        (CommitHub)
+      </a>
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.footer {
+  text-align: center;
+}
+</style>
