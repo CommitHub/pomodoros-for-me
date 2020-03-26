77c5a8ba9bdde7cc9a10dab2ef611e78ef2063a5
Luis <luis.alvarez@commithub.com>
2020-03-26 09:38:24 -0400

    setup for firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import * as firebase from "firebase/app";
+import dotenv from 'dotenv';
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
