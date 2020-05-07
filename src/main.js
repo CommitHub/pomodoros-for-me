7770d765cc1cc9f81f2caf50b42310fba8a14244
Luis <luis.alvarez@commithub.com>
2020-05-07 15:29:04 -0400

    showcasing test pomodoros in metrics page

--- src/main.js
+++ src/main.js
@@ -1,27 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
 import "./registerServiceWorker";
-import firebaseConfig from "./firebase/firebaseConfig.js";
 
 Vue.config.productionTip = false;
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-
-db.collection("pomodoros")
-  .get()
-  .then((querySnapshot) => {
-    querySnapshot.forEach(doc => {
-      console.log(doc.data())
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
-  });
 
 new Vue({
   router,
--- src/views/Metrics.vue
+++ src/views/Metrics.vue
@@ -1,5 +1,35 @@
 <template>
   <section id="metrics">
     <h1>Metrics</h1>
+    {{ pomodoros }}
   </section>
 </template>
+
+<script>
+import db from "../firebase/firebaseInit.js";
+
+export default {
+  name: "metrics",
+  data() {
+    return {
+      pomodoros: []
+    };
+  },
+  created() {
+    db.collection("pomodoros")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = {
+            id: doc.id,
+            date: doc.data().date,
+            types: doc.data().types,
+            amount: doc.data().amount
+          };
+
+          this.pomodoros.push(data);
+        });
+      });
+  }
+};
+</script>
